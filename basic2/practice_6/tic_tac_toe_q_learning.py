db9362cffc1acbaf23443babd29b0cd7a635079e
Youn-Hee Han <yh21.han@gmail.com>
2020-09-14 01:12:17 +0900

    ...

--- basic2/practice_6/tic_tac_toe_q_learning.py
+++ basic2/practice_6/tic_tac_toe_q_learning.py
@@ -8,10 +8,10 @@ from basic2.practice_6.tic_tac_toe_utils import print_step_status, print_game_st
 
 INITIAL_EPSILON = 1.0
 FINAL_EPSILON = 0.01
-LAST_SCHEDULED_EPISODES = 100000
+LAST_SCHEDULED_EPISODES = 50000
 
 # 최대 반복 에피소드(게임) 횟수
-MAX_EPISODES = 1000000
+MAX_EPISODES = 500000
 
 STEP_VERBOSE = False
 BOARD_RENDER = False
