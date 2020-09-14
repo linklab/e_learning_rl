8ab69c701a3102700754b790961acd5c271b28eb
Youn-Hee Han <yh21.han@gmail.com>
2020-09-14 13:14:57 +0900

    ...

--- basic2/practice_6/tic_tac_toe_agents.py
+++ basic2/practice_6/tic_tac_toe_agents.py
@@ -6,31 +6,7 @@ ALPHA = 0.1
 # 감가율
 GAMMA = 0.99
 
-
-class Human_Agent:
-    def __init__(self, name, env):
-        self.name = name
-        self.env = env
-
-    def get_action(self, current_state):
-        available_actions_ids = current_state.get_available_actions()
-        valid_action_id = False
-        action_id = None
-
-        while not valid_action_id:
-            action_id = int(input("9개 셀 중 하나를 선택하세요 (보드 상단부터 숫자 키패드와 매칭하여 [7,8,9, 4,5,6, 1,2,3] 숫자 중 하나를 선택하고 엔터를 누르세요)"))
-            if action_id > 9 or action_id < 0:
-                print("[입력 오류: {0}] 1부터 9사이의 숫자 값을 입력하세요.".format(action_id))
-                continue
-
-            if action_id not in available_actions_ids:
-                print("[입력 오류: {0}] 유효한 셀을 선택하세요.".format(action_id))
-            else:
-                valid_action_id = True
-
-        return action_id
-
-
+#Q-Learning 에이전트
 class Q_Learning_Agent:
     def __init__(self, name, env):
         self.name = name
@@ -147,3 +123,27 @@ class Q_Learning_Agent:
             state_policy_list.append("{0}:{1:.3f}".format(action_id, action_probs[idx]))
 
         return ", ".join(state_policy_list)
+
+
+class Human_Agent:
+    def __init__(self, name, env):
+        self.name = name
+        self.env = env
+
+    def get_action(self, current_state):
+        available_actions_ids = current_state.get_available_actions()
+        valid_action_id = False
+        action_id = None
+
+        while not valid_action_id:
+            action_id = int(input("9개 셀 중 하나를 선택하세요 (보드 상단부터 숫자 키패드와 매칭하여 [7,8,9, 4,5,6, 1,2,3] 숫자 중 하나를 선택하고 엔터를 누르세요)"))
+            if action_id > 9 or action_id < 0:
+                print("[입력 오류: {0}] 1부터 9사이의 숫자 값을 입력하세요.".format(action_id))
+                continue
+
+            if action_id not in available_actions_ids:
+                print("[입력 오류: {0}] 유효한 셀을 선택하세요.".format(action_id))
+            else:
+                valid_action_id = True
+
+        return action_id
